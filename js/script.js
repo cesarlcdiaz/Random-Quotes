077f426db456a403a7faba0500e3a5f42ac53310
cesar diaz <cesarlcdiaz@gmail.com>
2016-08-18 14:45:20 -0700

    #2 user can generate random quote

--- js/script.js
+++ js/script.js
@@ -1,37 +1,35 @@
 // here is where I stored the quotes 
-   
-   var quoteOne = {
+  var quotes = [   
+    {
         quote : 'I meant what I said and I said what I meant.',
         source : 'Dr. Seuss',
         citation : 'dont know',
         year : '1990'
     },
-        quoteTwo = {
+    {
         quote : 'Truth is everybody is going to hurt you: you just gotta find the     ones worth suffering for.',
         source : 'Bob Marley',
         citation : 'Smoke session',
         year : '1989'
     },
-        quoteThree = {
+    {
         quote : 'Never interrupt your enemy when he is making a mistake.',
         source : 'Napoleon Bonaparte',
         citation : 'I dont know',
         year : '1991'
     },
-        quoteFour = {
+    {
         quote : 'Fear is the main source of superstition, and one of the main sources of cruelty. To conquer fear is the beginning of wisdom.',
         source : 'Bertrand Russell',
         citation : 'I dont know',
         year : '1992'
     },
-    quoteFive = {
+    {
         quote : 'Curiosity will conquer fear even more than bravery will.',
         source : 'James Stephens',
         citation : 'I dont know',
         year : '1993'
-    };
-//this is the variable that holds the array of quotes.
-  var quotes = [quoteOne, quoteTwo, quoteThree, quoteFour,quoteFive];
+    }];
 //this function picks a quote in random from my quote storage and returns how to print on the page.
     function getRandomQuote() {
         var item = quotes[Math.floor(Math.random()* quotes.length)];
